61b52b0a67ddfb7d7419924a64492097371e5338
Ahmedelmalki <elmalkiahmed02@gmail.com>
2024-05-07 00:42:20 +0100

    kkk

--- ispowerof2/main.go
+++ ispowerof2/main.go
@@ -1,12 +1,33 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
-func main(){
-	num := os.Args[1]
-
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("plz check ur input agian")
+	} else {
+		num, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			fmt.Println("invalid input")
+		} else {
+			fmt.Println(ispower2(num))
+		}
+	}
 }
 
-func ispower(n int) bool {
-	
+func ispower2(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	for n%2 == 0 {
+		n /= 2
+		if n == 1 {
+			return true
+		}
+	}
+	return false
 }
