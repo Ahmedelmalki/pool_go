b49f095f585a4afdfc9a38e2ba15d46a216af341
Ahmedelmalki <elmalkiahmed02@gamil.com>
2024-08-05 19:35:06 +0100

    pri;e

--- fprime/main.go
+++ fprime/main.go
@@ -5,46 +5,17 @@ import (
 )
 
 func main() {
-	fmt.Println(fprime(8333325))
+	fprime(8333325)
 }
 
-func fprime(nums int) string {
-	rs := []string{}
-	if nums == 1 {
-		return "1"
-	}
-	for i := 2; i*i <= nums; i++ {
-		for nums%i == 0 {
-			rs = append(rs, myitoa(i))
+func fprime(nums int) {
+	i := 2
+	for nums > 1 {
+		if nums%i == 0 {
+			fmt.Print(i, "*")
 			nums /= i
+		} else {
+			i++
 		}
 	}
-	if nums > 1 {
-		rs = append(rs, myitoa(nums))
-	}
-	return myjion(rs, "*")
-}
-
-func myjion(slc []string, sep string) string {
-	if len(slc) == 0 {
-		return ""
-	}
-	if len(slc) == 1 {
-		return slc[0]
-	}
-	rs := ""
-	for i := 0; i < len(slc)-1; i++ {
-		rs = rs + slc[i] + sep
-	}
-	rs += slc[len(slc)-1]
-	return rs
-}
-
-func myitoa(n int) string {
-	rs := ""
-	for n != 0 {
-		rs = string(n%10+48) + rs
-		n /= 10
-	}
-	return rs
 }
