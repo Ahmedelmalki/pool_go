90c4a2d915c83dbcf39e9277444179bc684e53ad
Ahmedelmalki <elmalkiahmed02@gmail.com>
2024-08-10 13:41:17 +0000

    zip

---
+++ zipstring/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests/solutions"
+)
+
+func main() {
+	args := []string{
+		"aaaa",
+		"zzzzzZZZZZZ",
+		"",
+		"ziiiiipMeee",
+		"hhellloTthereYouuunggFelllas",
+	}
+
+	for _, arg := range args {
+		challenge.Function("ZipString", ZipString, solutions.ZipString, arg)
+	}
+}
+
+func ZipString(s string) string {
+	if len(s) == 0{
+		return s
+	}
+	rs := ""
+	c := 1
+	for i := 0; i < len(s)-1; i++ {
+		if s[i] == s[i+1] {
+			c++
+		} else {
+			rs += string(c+48) + string(s[i])
+			c = 1
+		}
+	}
+	rs += string(c+48) + string(s[len(s)-1])
+	return rs
+}
