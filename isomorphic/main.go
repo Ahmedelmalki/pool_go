b2d73dd7a20af266a87093ca94e18eed93a6e5f7
Ahmedelmalki <elmalkiahmed02@gmail.com>
2024-08-16 15:15:03 +0000

    isomorphic

--- isomorphic/main.go
+++ isomorphic/main.go
@@ -1,9 +1,25 @@
 package main
 
-func main(){
+import "fmt"
 
+func main() {
+	// fmt.Println(isIsomorphic("",""))
+	fmt.Println(isIsomorphic("ab", "cd"))
+	 fmt.Println(isIsomorphic("ufdw","ffuy"))
 }
 
-func isomorphic(s ,t string) bool{
-	
+func isIsomorphic(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	mapS := make(map[byte]byte)
+	mapT := make(map[byte]byte)
+	for i := 0; i < len(s); i++ {
+		if mapS[t[i]] > 0 && mapS[t[i]] != s[i] || mapT[s[i]] > 0 && mapT[s[i]] != t[i] {
+			return false
+		}
+		mapS[t[i]] = s[i]
+		mapT[s[i]] = t[i]
+	}
+	return true
 }
