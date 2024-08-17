5e6dfdc57ae52631d745f714424c1aff698b5c8c
Ahmedelmalki <elmalkiahmed02@gmail.com>
2024-08-17 16:38:51 +0000

    slice

--- slice/main.go
+++ slice/main.go
@@ -1,77 +1,86 @@
 package main
 
 import (
-
 	"github.com/01-edu/go-tests/lib/challenge"
 	"github.com/01-edu/go-tests/lib/chars"
 	"github.com/01-edu/go-tests/lib/random"
 	"github.com/01-edu/go-tests/solutions"
 )
-
 func main() {
-	elems := [][]interface{}{
-		{
-			[]string{"coding", "algorithm", "ascii", "package", "golang"},
-			1,
-		},
-		{
-			[]string{"coding", "algorithm", "ascii", "package", "golang"},
-			-3,
-		},
-		{
-			[]string{"coding", "algorithm", "ascii", "package", "golang"},
-			2, 4,
-		},
-		{
-			[]string{"coding", "algorithm", "ascii", "package", "golang"},
-			-2, -1,
-		},
-		{
-			[]string{"coding", "algorithm", "ascii", "package", "golang"},
-			2, 0,
-		},
+	elems := []struct {
+		a    []string
+		nbrs []int
+	}{
+		{[]string{"coding", "algorithm", "ascii", "package", "golang"}, []int{1}},
+		{[]string{"coding", "algorithm", "ascii", "package", "golang"}, []int{-3}},
+		{[]string{"coding", "algorithm", "ascii", "package", "golang"}, []int{2, 4}},
+		{[]string{"coding", "algorithm", "ascii", "package", "golang"}, []int{-2, -1}},
+		{[]string{"coding", "algorithm", "ascii", "package", "golang"}, []int{2, 0}},
 	}
 
-	s := random.StrSlice(chars.Words)
-
-	elems = append(elems, []interface{}{s, -len(s) - 10, -len(s) - 5})
-
+	// Add random test cases
 	for i := 0; i < 3; i++ {
-		s = random.StrSlice(chars.Words)
-		elems = append(elems, []interface{}{s, random.IntBetween(-len(s)-10, len(s)+10), random.IntBetween(-len(s)-8, len(s)+10)})
+		s := random.StrSlice(chars.Words)
+		elems = append(elems, struct {
+			a    []string
+			nbrs []int
+		}{
+			s,
+			[]int{random.IntBetween(-len(s)-10, len(s)+10), random.IntBetween(-len(s)-8, len(s)+10)},
+		})
 	}
 
-	for _, a := range elems {
-		challenge.Function("Slice", Slice, solutions.Slice, a...)
+	for _, e := range elems {
+		// Unpack both the slice and the variadic integers for the function call
+		args := append([]interface{}{e.a}, interfaceSlice(e.nbrs)...)
+		challenge.Function("Slice", Slice, solutions.Slice, args...)
 	}
 }
 
+// Convert []int to []interface{}
+func interfaceSlice(slice []int) []interface{} {
+	interSlice := make([]interface{}, len(slice))
+	for i, v := range slice {
+		interSlice[i] = v
+	}
+	return interSlice
+}
+
+
 func Slice(a []string, nbrs ...int) []string {
-	lenght := len(a)
-	if len(nbrs) ==0 {
+	length := len(a)
+
+	if len(nbrs) == 0 {
 		return nil
 	}
+
 	start := nbrs[0]
-	end := lenght
-	if len(nbrs) == 1 {
-		end = lenght
-	} else {
-		end = nbrs[len(nbrs)-1]
+	end := length
+
+	if len(nbrs) > 1 {
+		end = nbrs[1]
 	}
+
+	// Handle negative indices
 	if start < 0 {
-		start = lenght + start
+		start = length + start
 	}
 	if end < 0 {
-		end = lenght + end 
+		end = length + end
 	}
+
+	// Ensure start and end are within bounds
 	if start < 0 {
 		start = 0
 	}
-	if end > lenght{
-		end = lenght
+	if end > length {
+		end = length
 	}
-	if start > end  {
+
+	// If start is greater than end, return nil
+	if start > end {
 		return nil
 	}
+
 	return a[start:end]
 }
