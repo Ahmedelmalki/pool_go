7913ef5103bc06a0d1c1126eae9147c52c7fa18d
Ahmedelmalki <elmalkiahmed02@gmail.com>
2024-08-27 19:19:44 +0100

    linked list

--- linked_lists/main.go
+++ linked_lists/main.go
@@ -2,48 +2,97 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-type node struct {
-	value int
-	next  *node
+type Node struct {
+	data int
+	next *Node
 }
 
-type linkedlist struct {
-	head *node
-	len  int
+type Linkedlist struct {
+	head *Node
 }
 
-func (l *linkedlist) add(value int) {
-	newNode := new(node)
-	newNode.value = value
-	if l.head == nil {
-		l.head = newNode
-	} else {
-		iterator := l.head
-		for ; iterator != nil; iterator = iterator.next {
-		}
-		iterator.next = newNode
+func (list *Linkedlist) insertAtBack(data int) {
+	newNode := &Node{data: data, next: nil}
+
+	if list.head == nil {
+		list.head = newNode
+		return
 	}
+
+	current := list.head
+	for current.next != nil {
+		current = current.next
+	}
+	current.next = newNode
 }
 
-// func (l *linkedlist) remove(value int) {
+func (list *Linkedlist) insertFront(data int) {
+	if list.head == nil {
+		newNode := &Node{data: data, next: nil}
+		list.head = newNode
+		return
+	}
 
-// }
+	newNode := &Node{data: data, next: list.head}
+	list.head = newNode
+}
 
-// func (l *linkedlist) get(value int) *node {
+func (list *Linkedlist) insertAfterValue(afterValue, data int) {
+	newNode := &Node{data: data, next: nil}
 
-// }
+	current := list.head
 
-func (l *linkedlist) String() string{
-	sb := strings.Builder{}
+	for current != nil {
+		if current.data == afterValue {
+			newNode.next = current.next
+			current.next = newNode
+			return
+		}
+		current = current.next
+	}
+	fmt.Printf("cannot insert node, after value %d doesn't exist", afterValue)
+	fmt.Println()
+}
+
+func (list *Linkedlist) insertBeforeValue(beforeValue, data int) {
+	if list.head == nil {
+		return
+	}
+
+	if list.head.data == beforeValue {
+		newNode := &Node{data: data, next: list.head}
+		list.head = newNode
+		return
+	}
+
+	current := list.head
+	for current.next != nil {
+		if current.next.data == beforeValue {
+			newNode := &Node{data: data, next: current.next}
+			current.next = newNode
+			return
+		}
+		current = current.next
+	}
+	fmt.Printf("cannot insert node, before value %d doesn't exist", beforeValue)
+	fmt.Println()
+}
+
+func (list *Linkedlist) print() {
+	var current *Node = list.head
+	for current != nil {
+		fmt.Printf("%d -> ", current.data)
+		current = current.next
+	}
+	fmt.Println()
 }
 
 func main() {
-	fmt.Println("hello world")
-
-	l := linkedlist{}
-	l.add(1)
-	fmt.Println(l)
+	myList := Linkedlist{}
+	myList.insertFront(10)
+	myList.insertFront(20)
+	myList.insertFront(30)
+	myList.print()
 }
